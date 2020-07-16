97a753b7c6ce24c984fe6b1c7bf9dfb41f1ba740
Warda Ayaz <wayaz107@gmail.com>
2020-07-16 23:40:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,7 +5,7 @@ your_hash
 end
 
 
-def shipping_manifest(the_manifest)
+def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
