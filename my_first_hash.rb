a6d11508fb9a1405d5f3003e0b255f67af6aff0c
Warda Ayaz <wayaz107@gmail.com>
2020-07-16 23:33:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash(your_hash)
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-{"my_name" => "Harry Potter"}
+test_hash = {}
 end
 
 
